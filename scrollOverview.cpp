1a593f33d17af9078cace80b411739965ffec3e0
Daniel Skorupa <daniel.skorupa@yayuuu.pl>
2026-07-30 20:09:15 +0200

    fix: clear window focus when switching workspace, prevents unintentionally switching back workspace when interacting with other layers

--- scrollOverview.cpp
+++ scrollOverview.cpp
@@ -3407,6 +3407,8 @@ void CScrollOverview::syncSelectionToViewport() {
     }
 
     closeOnWindow.reset();
+    if (activeScrollOverview().get() == this)
+        Desktop::focusState()->fullWindowFocus(nullptr, Desktop::FOCUS_REASON_DESKTOP_STATE_CHANGE);
 }
 
 void CScrollOverview::syncFocusedSelection() {
